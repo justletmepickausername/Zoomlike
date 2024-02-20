7f24260f5c8085e25f26591bdf38de6266ae8f38
Matt <forcatalina@gmail.com>
2024-02-20 18:30:20 +0200

    fcking hell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -51,7 +50,6 @@ import { TeacherDashboardPageFinalComponent } from './teacher-dashboard-page-fin
     HttpClientModule,
     MatIconModule,
     MatSelectModule,
-    BrowserAnimationsModule,
     MatButtonToggleModule,
     NgxMaterialTimepickerModule,
     MatSliderModule
